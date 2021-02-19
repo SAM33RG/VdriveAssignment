f624975575384318bcb84f8e4efd77b907c529f8
sam33r <sameergangar1@gmail.com>
2021-02-19 21:17:35 +0530

    Boilerplate

---
+++ app/src/main/java/tech/sam33rg/vdriveassignment/MainActivity.java
@@ -0,0 +1,14 @@
+package tech.sam33rg.vdriveassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
